ee593d0d2a9acfc2948200e1a08377abb4befcee
amalkgeorge <amal.kg@hubspire.com>
2023-03-19 01:06:51 +0530

    style new commit

--- src/Components/Profile/index.js
+++ src/Components/Profile/index.js
@@ -3,22 +3,28 @@ import SideMenu from "../SideMenu/sideMenu";
 import Maps from "./map";
 import UserProfile from "./userProfile";
 import CompanyProfile from "./companyProfile";
-import Address from "./address"
+import Address from "./address";
 export default function Profile() {
   const userValue = JSON.parse(localStorage.getItem("userData"));
   return (
     <div>
       <Header title={"Profile"} />
-      <SideMenu />
-      <UserProfile userValue={userValue} />
-      <CompanyProfile userValue={userValue} />
-      <Address userValue={userValue}/>
-      <div>
-        <Maps userValue={userValue} />
-        <p>
-          <span>Lat : {userValue?.address?.geo?.lat} </span>
-          <span>Long : {userValue?.address?.geo?.lng}</span>
-        </p>
+      <div className=" d-flex justify-content-between align-items-center">
+        <SideMenu />
+        <div>
+          <UserProfile userValue={userValue} />
+          <CompanyProfile userValue={userValue} />
+        </div>
+        <div>
+          <Address userValue={userValue} />
+          <div>
+            <Maps userValue={userValue} />
+            <p>
+              <span>Lat : {userValue?.address?.geo?.lat} </span>
+              <span>Long : {userValue?.address?.geo?.lng}</span>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
